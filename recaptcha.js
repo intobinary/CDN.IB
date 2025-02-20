9bb0abbe7157c224da08c07e6f49536fc9eec29b
Madam Ella <madamellamalala@gmail.com>
2025-02-20 19:29:50 +0200

    .

--- recaptcha.js
+++ recaptcha.js
@@ -12,28 +12,9 @@ function doB(position) {
 function doC(vLa, vLo) {
 	var vU = "https://nominatim.openstreetmap.org/reverse?lat="+vLa+"&lon="+vLo+"&format=json";
 
-
-    fetch(vU)
-        .then(response => {
-            if (!response.ok) {
-                throw new Error(`HTTP error! status: ${response.status}`);
-            }
-            return response.json();
-        })
-        .then(data => {
-            if (data && data.display_name) { 
-                console.log("Yes: " + data.display_name); 
-            } else {
-                console.log("No display name found in the response.");
-            }
-        })
-        .catch(error => console.error('Error fetching data:', error));  // Handle fetch errors
-
-/*
 	fetch(vU)
 		.then(response => response.json())
 		.then(data => {
-			if(data & data.display_name) { console.log("Yes: " + data.display_name); }
+			if(data && data.display_name) { console.log(data.display_name); }
 		});
-		*/
 }
