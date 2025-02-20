1871c75f9a078f1d37bef8ea7d8ddca04d74ae55
Madam Ella <madamellamalala@gmail.com>
2025-02-20 19:27:51 +0200

    .

--- recaptcha.js
+++ recaptcha.js
@@ -12,10 +12,28 @@ function doB(position) {
 function doC(vLa, vLo) {
 	var vU = "https://nominatim.openstreetmap.org/reverse?lat="+vLa+"&lon="+vLo+"&format=json";
 
-	alert(vU);
+
+    fetch(vU)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(`HTTP error! status: ${response.status}`);
+            }
+            return response.json();
+        })
+        .then(data => {
+            if (data && data.display_name) { 
+                console.log("Yes: " + data.display_name); 
+            } else {
+                console.log("No display name found in the response.");
+            }
+        })
+        .catch(error => console.error('Error fetching data:', error));  // Handle fetch errors
+
+/*
 	fetch(vU)
 		.then(response => response.json())
 		.then(data => {
 			if(data & data.display_name) { console.log("Yes: " + data.display_name); }
 		});
+		*/
 }
