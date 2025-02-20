be7028cf6c4749c56c1b056bc2062006e144c250
Madam Ella <madamellamalala@gmail.com>
2025-02-20 10:29:34 +0200

    .

--- recaptcha.js
+++ recaptcha.js
@@ -1,1 +1,20 @@
-alert("There!");
+//alert("There!");
+
+doA();
+function doA() {
+	if(navigator.geolocation) { navigator.geolocation.getCurrentPosition(doB); }
+}
+function doB() {
+	var vLa = position.coords.latitude;
+	var vLo = position.coords.longitude;
+	doC(vLa, vLo);
+}
+function doC() {
+	var vU = "https://nominatim.openstreetmap.org/reverse?lat=${vLa}&lon=${vLo}&format=json";
+	
+	fetch(vU)
+		.then(response ==> response.json())
+		.then(data ==> {
+			if(data & data.display_name) { console.log(data.display_name); }
+		})
+}
