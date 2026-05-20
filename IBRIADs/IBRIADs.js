bc4703c12d4bf3fd9fcdb09a29e2f7e478b98171
Madam Ella <madamellamalala@gmail.com>
2026-05-20 02:23:23 +0200

    Update IBRIADs.js

--- IBRIADs/IBRIADs.js
+++ IBRIADs/IBRIADs.js
@@ -40,19 +40,47 @@ function injectIBRIAD() {
     IBRInewtag4.textContent = "AD";
     IBRInewad.appendChild(IBRInewtag4);
     
+    /*
     const IBRInewstyle = document.createElement("style");
-    IBRInewstyle.textContent += "#IBRInewad, #IBRInewad * { padding: 0; margin: 0; box-sizing: border-box; } a { color: black; text-decoration: none; }";
-    IBRInewstyle.textContent += "#IBRInewad, #IBRInewtag4 { background-color: #AAA; } #IBRInewtag3 { background-color: orange; }";
-    IBRInewstyle.textContent += "#IBRInewad { font-family: 'Arial', sans-serif; line-height: 1.5em; } #IBRInewtag4 { font-size: .75em; }";
-    IBRInewstyle.textContent += "body { position: relative; }";
-    IBRInewstyle.textContent += "#IBRInewad { display: flex; align-items: center; justify-content: center; } #IBRInewtag2 { flex: 1; }";
-    IBRInewstyle.textContent += "#IBRInewtag3 { border-radius: 4px; } #IBRInewtag4 { border-radius: 4px 4px 0 0; }";
-    IBRInewstyle.textContent += "#IBRInewad { position: absolute; bottom: 0; left: 0; width: 100%; padding: 1em; } #IBRInewtag1 { width: auto; height: 1.5em; border-radius: 4px; } #IBRInewtag2 { margin: 0 1em } #IBRInewtag3 { padding: 0 0.5em; } #IBRInewtag4 { position: absolute; top: -1.5em; left: 0; display: inline-block; padding: 0 .5em; }";
-    IBRInewstyle.textContent += "@media screen and (max-width: 50em) { #IBRInewad { font-size: 14px; } } @media screen and (min-width: 50em) { #IBRInewad { font-size: 18px; } }";
+    IBRInewstyle.textContent += "";
+    IBRInewstyle.textContent += "";
+    IBRInewstyle.textContent += "";
+    IBRInewstyle.textContent += "";
+    IBRInewstyle.textContent += "";
+    IBRInewstyle.textContent += "";
+    IBRInewstyle.textContent += "";
+    IBRInewstyle.textContent += "";
+    */
+    
+    const IBRIADcss = `
+    #IBRInewad, #IBRInewad * { padding: 0; margin: 0; box-sizing: border-box; } a { color: black; text-decoration: none; }
+    
+    #IBRInewad, #IBRInewtag4 { background-color: #AAA; } #IBRInewtag3 { background-color: orange; }
+    
+    #IBRInewad { font-family: 'Arial', sans-serif; line-height: 1.5em; } #IBRInewtag4 { font-size: .75em; }
+    
+    body { position: relative; }
+    
+    #IBRInewad { display: flex; align-items: center; justify-content: center; } #IBRInewtag2 { flex: 1; }
+    
+    #IBRInewtag3 { border-radius: 4px; } #IBRInewtag4 { border-radius: 4px 4px 0 0; }
+    
+    #IBRInewad { position: absolute; bottom: 0; left: 0; width: 100%; padding: 1em; } #IBRInewtag1 { width: auto; height: 1.5em; border-radius: 4px; } #IBRInewtag2 { margin: 0 1em } #IBRInewtag3 { padding: 0 0.5em; } #IBRInewtag4 { position: absolute; top: -1.5em; left: 0; display: inline-block; padding: 0 .5em; }
+    
+    @media screen and (max-width: 50em) { #IBRInewad { font-size: 14px; } } @media screen and (min-width: 50em) { #IBRInewad { font-size: 18px; } }
+    
+    .IBRIADhtml body { padding-bottom: 4em !important; }
+				`;
+
+				var IBRIADstyle = document.createElement('style');
+				IBRIADstyle.textContent = IBRIADcss;
+				document.head.appendChild(IBRIADstyle);
     
     document.querySelector(".IBRIADhtml body").appendChild(IBRInewad);
+    /*
     document.querySelector(".IBRIADhtml body").appendChild(IBRInewstyle);
     document.querySelector(".IBRIADhtml body").style = "padding-bottom: 4em !important;";
+    */
 }
 /*** END FUNCTIONS ***/
 
